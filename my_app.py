62e9071dae71543da41c836749d33f91b9ba7add
P1rozhenka <robinzon749@gmail.com>
2025-03-29 19:02:08 +0500

    1

--- my_app.py
+++ my_app.py
@@ -1,28 +1,34 @@
 # напиши здесь код основного приложения и первого экрана
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton
+from instr import *
+from second_win import TestWin
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.move(win_width, win_height)
+        self.resize(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text, alignment= Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment= Qt.AlignLeft)
+        self.layout.addWidget(self.button, alignment= Qt.AlignCenter)
+        self.setLayout(self.layout)
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
 
 app = QApplication([])
-win = QWidget()
-win.setWindowTitle('Мое первое приложение!')
-win.move(900, 70)
-win.resize(400, 200)
-
-text1 = QLabel('Добро пожаловать в программу по определению состояния здоровья!')
-text2 = QLabel('Данное приложение позволит вам с помощью тесте Руфье провести первичную диагностику вашего здоровья.' /n"Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособности сердца при физической нагрузке." /n"У испытуемого, находящегося в положении лежа на спине в течение 5 мин, определяют частоту пульса за 15 секунд;" /n"затем в течение 45 секунд испытуемый выполняет 30 приседаний." /n"После окончания нагрузки испытуемый ложится, и у него вновь подсчитывается число пульсаций за первые 15 секунд," /n"а потом - за последние 15 секунд первой минуты периода восстановления." /n"Важно! Если в процессе проведения испытания вы почуствуете себя плохо (появится головокружение, шум в" /n"ушах, сильная одышка и др.), то тест необходимо прервать и обратиться к врачу. ")
-
-line = QVBoxLayout()
-h1 = QHBoxLayout()
-h2 = QHBoxLayout()
-h3 = QHBoxLayout()
-
-h1.addWidget(text1, alignment=Qt.AlignLeft)
-h2.addWidget(text2, alignment=Qt.AlignLeft)
-
-line.addLayout(h1)
-line.addLayout(h2)
-line.addLayout(h3)
-win.setLayout(line)
-
-win.show()
+mv = MainWin()
 app.exec_()
--- second_win.py
+++ second_win.py
@@ -1,1 +1,40 @@
 # напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton
+from instr import *
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.move(win_width, win_height)
+        self.resize(win_x, win_y)
+    def initUI(self):
+        self.name = QLabel(txt_name)
+        self.hintname = QLineEdit(txt_hintage)
+        self.age = QLabel(txt_age)
+        self.hintage = QLineEdit(txt_hintage)
+        self.test1 = QLabel(txt_test1)
+        self.starttest1 = QPushButton(txt_starttest1)
+        self.hinttest1 = QLineEdit(txt_hinttest1)
+        self.test2 = QLabel(txt_test2)
+        self.starttest2 = QPushButton(txt_starttest2)
+        self.test3 = QLabel(txt_test3)
+        self.starttest3 = QPushButton(txt_starttest3)
+        self.hinttest2 = QLineEdit(txt_hinttest2)
+        self.hinttest3 = QLineEdit(txt_hinttest3)
+        self.button = QPushButton(txt_sendresults)
+        self.text_timer = QLabel(txt_timer)
+        self.l_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.h_line = QHBoxLayout()
+        self.l_line.addWidget(self.hello_text, alignment= Qt.AlignLeft)
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+    def next_click(self):
+        self.tw = FinalWin()
+        self.hide
